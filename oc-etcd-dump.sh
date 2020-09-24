9413617abb9480f8cf47778ac2aa7c117a8a4f8c
Michal Minář <miminar@redhat.com>
2020-09-24 05:17:31 +0200

    oc-etcd-dump
    Signed-off-by: Michal Minář <miminar@redhat.com>

---
+++ oc-etcd-dump.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+USAGE="${BASH_SOURCE[0]} [-h]
+
+Requires GNU parallel.
+
+Dump all the objects in OCP's etcd as both yaml and json files in the following format:
+
+    <resource-name>.<api-group>.(yaml|json)
+
+Each as a list of resources.
+
+Options:
+  -h        Print this message and exit.
+"
+
+if [[ "${1:-}" =~ ^(-h|--help)$ ]]; then
+    printf '%s' "$USAGE"
+    exit 0
+elif [[ "$#" -gt 0 ]]; then
+    printf 'Unknown option!\n' >&2
+    exit 1
+fi
+
+#readarray -t projects <<<"$(oc get project -o \
+    #jsonpath=$'{range .items[*]}{.metadata.name}\n{end}')"
+
+readarray -t resources <<<"$(oc api-resources -o wide | awk '{
+    if (NR == 1) {
+        if ((groupIndex = index($0, "APIGROUP")) < 1) {
+            print "Failed to find APIGROUP column!" >/dev/stderr
+            exit 1
+        }
+        next
+    }
+    if ($0 ~ /\[.*\<list\>.*\]/) {
+        if (substr($0, groupIndex, 1) == " ") {
+            print $1
+        } else {
+            name = $1
+            $0 = substr($0, groupIndex)
+            OFS = "."
+            print  name, $1
+        }
+    }
+}')"
+
+parallel --files --results '{1}.{2}' oc get -o '{2}' '{1}' --all-namespaces \
+    ::: "${resources[@]}" ::: json yaml
+
+printf '\nPruning the following output files:\n'
+find . -maxdepth 1 -type f \( \( -regex '.*\.\(yaml\|json\)\(\.err\)?' -empty \) \
+    -o -regex '.*\.\(yaml\|json\)\.seq' \) -print -delete
